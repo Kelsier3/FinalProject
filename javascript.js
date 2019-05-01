1847339544b9c3b4fd15a7239710e8a23250611b
Kelsier3 <47090259+Kelsier3@users.noreply.github.com>
2019-05-01 17:00:56 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// toggles showing the dropdown content and download links 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//I don't know if I could have used JSON for this. Only just realized it at the end when I am trying to figure out how to print it. //
